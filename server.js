5be69f64148945e2ff52df4ffb54c94894d09494
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-12-02 12:53:05 -0800

    basic angular build process is up

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var fs = require('fs');
 var unicornsRouter = require(__dirname + '/routes/unicorns_routes');
 
 app.get('/:filename', function(req, res, next) {
-  fs.stat(__dirname + '/build/' + req.params.filename, function(err, status) {
+  fs.stat(__dirname + '/build/' + req.params.filename, function(err, stats) {
     if (err) {
       console.log(err);
       return next();
     }
 
-    if(!stats.isFile()) return next();
+    if (!stats.isFile()) return next();
 
     var file = fs.createReadStream(__dirname + '/build/' + req.params.filename);
     file.pipe(res);
@@ -22,6 +22,6 @@ app.use(function(req, res) {
   res.status(404).send('could not find file');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(3000, function() {
   console.log('server up');
 });
