77b147e54e42b9a4d883bacf2d2c9616e775d2af
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-12-08 11:28:56 -0800

    added some tests ... still need to add a PUT and a DELETE test

--- test/client/unicorns_controller_tests.js
+++ test/client/unicorns_controller_tests.js
@@ -19,4 +19,37 @@ describe('unicorn controller', function() {
     expect(typeof controller).toBe('object');
     expect(Array.isArray($scope.unicorns)).toBe(true);
   });
+
+  describe('REST requests', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $ControllerConstructor('UnicornsController', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should add an array to unicorns with a Get all', function() {
+      $httpBackend.expectGET('/api/unicorns').respond(200, [{_id: 1, name: 'test unicorn'}]);
+      //write test to make sure errors are getting populated
+      $scope.getAll();
+      $httpBackend.flush();
+      expect($scope.unicorns[0].name).toBe('test unicorn');
+    });
+
+    it('should be able to create a new unicorn', function() {
+      $httpBackend.expectPOST('/api/unicorns', {name: 'test unicorn', color: 'rainbow', species: 'awesome'}).respond(200, {name: 'a different unicorn'});
+      expect($scope.unicorns.length).toBe(0);
+      // expect($scope.newUnicorn).toEqual($scope.defaults);
+      $scope.newUnicorn.name = 'test unicorn';
+      $scope.create($scope.newUnicorn);
+      $httpBackend.flush();
+      expect($scope.unicorns[0].name).toBe('a different unicorn');
+      // expect($scope.newUnicorn).toEqual($scope.defaults);
+      // see online code for augular.copy deal
+    });
+  });
 });
