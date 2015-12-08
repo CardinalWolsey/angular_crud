3e2278dde1ae7842aa2fc24a9fbe5fccdeb7f155
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-12-07 22:33:14 -0800

    added some css styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var minifyCss = require('gulp-minify-css');
-var contactCss = require('gulp-concat-css');
+var concatCss = require('gulp-concat-css');
 var gulpWatch = require('gulp-watch');
 
 gulp.task('static:dev', function() {
@@ -29,5 +29,20 @@ gulp.task('webpack:test', function() {
   .pipe(gulp.dest('test/client/'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev']);
+gulp.task('css:dev', function() {
+  return gulp.src([
+    'app/css/reset.css',
+    'app/css/base.css',
+    'app/css/layout.css',
+    'app/css/module.css'])
+  .pipe(concatCss('styles.min.css'))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('css:watch', function() {
+  gulp.watch('./app/css/**/*.css', ['css:dev']);
+});
+
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
 gulp.task('default', ['build:dev']);
