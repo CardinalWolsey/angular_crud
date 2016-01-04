ecf32393171d921adbc878417e73c96cbe7c25a1
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-01-04 08:06:02 -0800

    all tests written

--- test/client/unicorns_controller_tests.js
+++ test/client/unicorns_controller_tests.js
@@ -40,16 +40,48 @@ describe('unicorn controller', function() {
       expect($scope.unicorns[0].name).toBe('test unicorn');
     });
 
+//POST ... test not passing for unknown reason
     it('should be able to create a new unicorn', function() {
-      $httpBackend.expectPOST('/api/unicorns', {name: 'test unicorn', color: 'rainbow', species: 'awesome'}).respond(200, {name: 'a different unicorn'});
+      $httpBackend.expectPOST('/api/unicorns', {name: 'test unicorn'}).respond(200, {name: 'a different unicorn'});
       expect($scope.unicorns.length).toBe(0);
-      // expect($scope.newUnicorn).toEqual($scope.defaults);
+      expect($scope.newUnicorn).toBe(null);
       $scope.newUnicorn.name = 'test unicorn';
       $scope.create($scope.newUnicorn);
       $httpBackend.flush();
+      // expect($scope.newUnicorn).toBe(null);
       expect($scope.unicorns[0].name).toBe('a different unicorn');
-      // expect($scope.newUnicorn).toEqual($scope.defaults);
-      // see online code for augular.copy deal
     });
+
+//PUT
+    it('should be able to update a unicorn', function() {
+      $httpBackend.expectPUT('/api/unicorns/456').respond(200, 'successfully updated');
+      $scope.unicorns = [
+        {name: 'unicornOne'},
+        {name: 'unicornTwo', _id: '456'},
+        {name: 'unicornThree'}
+      ];
+      $scope.edit($scope.unicorns[1]);
+      expect($scope.unicorns[1].name).toBe('unicornThree');
+      expect($scope.editing).toBe(true);
+      expect($scope.newUnicorn.name).toBe('unicornTwo');
+      $scope.newUnicorn.name = 'unicornEdit';
+      $scope.submitEdit($scope.newUnicorn);
+      $httpBackend.flush();
+      expect($scope.Unicorns[2].name).toBe('unicornEdit');
+    });
+
+//DELETE ... tests not passing for some unknown reasion
+    it('should be able to delete a unicorn', function() {
+      $httpBackend.expectDelete('/api/unicorns/123').respond(200);
+      $scope.unicorns[0] = {
+        _id: 123,
+        name: 'test unicorn'
+      };
+      $scope.unicorns[1] = {name: 'otherUnicorn'};
+      $scope.remove($scope.unicorns[0]);
+      $httpBackend.flush();
+      expect($scope.unicorns[0].name).toBe('otherUnicorn');
+    });
+
   });
 });
